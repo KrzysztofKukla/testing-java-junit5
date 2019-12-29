0d5e227c38da3a5a827c95a6eda954d5cced2ede
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-12-29 22:55:33 +0100

    67 Test dependency injection with TestInfo, RepetitionInfo

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.RepeatedTest.CURRENT_REPETITION_PLACEHOLDER;
 import static org.junit.jupiter.api.RepeatedTest.DISPLAY_NAME_PLACEHOLDER;
@@ -36,5 +41,15 @@ class PersonTest implements ModelTest {
     @RepeatedTest(value = 10, name = DISPLAY_NAME_PLACEHOLDER + CURRENT_REPETITION_PLACEHOLDER)
     void repeatedTest() throws Exception {
 
+    }
+
+    @RepeatedTest(5)
+    //TestInfo and RepetitionInfo will be inject
+    void repeatedTestWithDependencyInjection(TestInfo testInfo, RepetitionInfo repetitionInfo) throws Exception {
+        String displayName = testInfo.getDisplayName();
+        Set<String> tags = testInfo.getTags();
+        System.out.println(displayName+"-> ");
+        tags.forEach((t)-> System.out.println("tag-> "+t));
+        System.out.println("current-> "+repetitionInfo.getCurrentRepetition());
     }
 }
