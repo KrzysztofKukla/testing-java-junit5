3fac82367a4514f442e8af8654a4eb289ce54b70
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-12-29 22:19:09 +0100

    65 @BeforeAll in interface test

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTest.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 /**
  * @author Krzysztof Kukla
  */
 @Tag("controller")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTest {
+
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("Before All from ControllerTest");
+    }
 }
