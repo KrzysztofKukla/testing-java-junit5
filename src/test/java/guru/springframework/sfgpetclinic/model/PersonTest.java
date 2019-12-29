efe0ceb9b950207ec2616939b0faebf3f0dc3e38
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-12-29 22:44:41 +0100

    66 @RepeatedTest

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -3,8 +3,13 @@ package guru.springframework.sfgpetclinic.model;
 import guru.springframework.sfgpetclinic.ModelTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.RepeatedTest.CURRENT_REPETITION_PLACEHOLDER;
+import static org.junit.jupiter.api.RepeatedTest.DISPLAY_NAME_PLACEHOLDER;
 
 /**
  * @author Krzysztof Kukla
@@ -25,5 +30,11 @@ class PersonTest implements ModelTest {
             () -> Assertions.assertEquals("Joe", person.getFirstName()),
             () -> Assertions.assertEquals("Buck", person.getLastName())
         );
+    }
+
+    @DisplayName("My Repeated test of->")
+    @RepeatedTest(value = 10, name = DISPLAY_NAME_PLACEHOLDER + CURRENT_REPETITION_PLACEHOLDER)
+    void repeatedTest() throws Exception {
+
     }
 }
